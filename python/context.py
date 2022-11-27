e4f4653eb60932f17970a3e151428a90c566c7a4
Gewton <gewtonj@br.ibm.com>
2022-11-27 12:22:35 -0500

    Save full path in playlist instead of internal URL reference

--- python/context.py
+++ python/context.py
@@ -1,9 +1,16 @@
 import sys
 import addtoplaylist
 
+def _generate_label(listitem):
+    videotitle = listitem.getVideoInfoTag().getTitle()
+    tvshowtitle = listitem.getVideoInfoTag().getTVShowTitle()
+    if tvshowtitle:
+        return "%s - %s" % (tvshowtitle, videotitle)
+    return videotitle
+
 if __name__ == '__main__':
-    path = sys.listitem.getPath()
-    label = sys.listitem.getLabel()
+    path = sys.listitem.getVideoInfoTag().getFilenameAndPath()
+    label = _generate_label(sys.listitem)
 
     if path and label:
         addtoplaylist.add_to_playlist(path, label)
